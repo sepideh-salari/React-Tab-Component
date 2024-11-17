20e0296da65e8f3f2dc46d61a208a10e1d00e84f
sepideh-salari <salari.sepideh@gmail.com>
2024-11-17 09:53:14 +0330

    Retrieve tab, style editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TabButtons from "./TabButtons";
 import TabContent from "./TabContent";
 import "./App.css";
 const App = () => {
-  const [activeTab, setActiveTab] = useState(0);
+  const savedTab = parseInt(localStorage.getItem("activeTab")) || 0;
+  const [activeTab, setActiveTab] = useState(savedTab);
   const tabData = [
     { label: "Home", content: "Welcome to the Home page!" },
     { label: "Profile", content: "This is your Profile page." },
     { label: "Settings", content: "Here you can change your Settings." },
     { label: "About", content: "Learn more About us here." },
   ];
-
+  useEffect(() => {
+    localStorage.setItem("activeTab", activeTab);
+  }, [activeTab]);
   return (
     <div className="main__container">
       <h1>Tab Component with React</h1>
